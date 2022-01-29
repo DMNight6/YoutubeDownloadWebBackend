a8bf09e003a22a1f508d594cbffe5f6af635b502
DMNight6 <50548798+DMNight6@users.noreply.github.com>
2022-01-29 20:31:01 +0800

    More fix kek
    Fix the link sorting (I am very pissed off :D)

--- Backend.js
+++ Backend.js
@@ -7,8 +7,11 @@ const App = express();
 var db = new sqlite3.Database('./database.db');
 
 const sort = async(data) => {
-    let video = await Youtube.getVideo(Youtube.validate(data) ? data : 'a');
-    return [{thumbnail: video.thumbnail.url, name: video.title, duration: video.durationFormatted, videoURL: video.url, id: 1}]
+    let id = await ytdl.getVideoID(data);
+    if (!id) return [];
+
+    let video = await Youtube.getVideo(`https://www.youtube.com/watch?v=${id}`)
+    return [{ thumbnail: video.thumbnail.url, name: video.title, duration: video.durationFormatted, videoURL: video.url, id: 1}]
 } // Sort one video only (For other application end.)
 
 const listSort = async(data) => {
@@ -22,6 +25,11 @@ const listSort = async(data) => {
     return List;
 } // Sort ten video into a format I wanted (For other application end.)
 
+const filter = (string) => {
+    const repl = /[#, %, &, { , }, \, <, >, *, ?, /, $, !, ', ", :, @, +, `, |, =]/g
+    if (!string.match(repl)) return string
+    else return string.replace(repl, " ")
+}
 App.use(require('cors')())
 
 App.get('/param', async(req, res) => {
@@ -49,16 +57,20 @@ App.get('/download', async(req, res) => {
     if (formats === '.mp3') {
         let file = fs.createWriteStream('video.mp3');
         ytdl(query, { filter: 'audio', quality: 'highestaudio' }).pipe(file);
-        file.once('finish', async() => {file.end(); res.sendFile(path.resolve('video.mp3'), (err) => { if (err) res.sendStatus(500); fs.unlinkSync(path.resolve('video.mp3')) });});
+        file.on('error', (err) => { file.destroy(err); res.sendStatus(500) })
+        file.on('finish', async() => { file.end(); res.sendFile(path.resolve('video.mp3'), (err) => { if (err) res.sendStatus(500) })})
+        file.on('close', () => { if (fs.existsSync(path.resolve('video.mp3'))) fs.unlinkSync(path.resolve('video.mp3')) })
     } else {
         let file = fs.createWriteStream('video.mp4');
         ytdl(query, { filter: 'videoandaudio', quality: 'highest'}).pipe(file)
-        file.once('finish', async() => {file.end(); res.sendFile(path.resolve('video.mp4'), (err) => { if(err) res.sendStatus(500); fs.unlinkSync(path.resolve('video.mp4')) });});
+        file.on('error', (err) => { file.destroy(err); res.sendStatus(500) })
+        file.on('finish', async() => { file.end(); res.sendFile(path.resolve('video.mp4'), (err) => { if (err) res.sendStatus(500) })})
+        file.on('close', () => { if (fs.existsSync(path.resolve('video.mp4'))) fs.unlinkSync(path.resolve('video.mp4')) })
     };
 }); // Sends file.
 
 App.get('/audioRender/', (req, res) => {
-    const [name, ext] = req.query.name.split('.');
+    const name = filter(req.query.name);
     const link = req.query.link;
 
     if (!link) return res.sendStatus(404);
@@ -68,12 +80,12 @@ App.get('/audioRender/', (req, res) => {
     let fs = require('fs');
     let path = require('path');
 
-    const file = fs.createWriteStream(path.resolve(`${name}.${ext}`));
+    const file = fs.createWriteStream(path.resolve(`${name}.mp3`));
     ytdl(link, {quality: 'highestaudio', filter: 'audioonly'}).pipe(file)
     
     file.on('error', (err) => { file.destroy(err); res.sendStatus(500) })
-    file.on('finish', () => { file.end(); res.sendFile(path.resolve(`${name}.${ext}`), function(err) { if (err) res.sendStatus(500); })});
-    file.on('close', () => { if (fs.existsSync(path.resolve(`${name}.${ext}`))) fs.unlinkSync(path.resolve(`${name}.${ext}`)) })
+    file.on('finish', () => { file.end(); res.sendFile(path.resolve(`${name}.mp3`), function(err) { if (err) res.sendStatus(500); })});
+    file.on('close', () => { if (fs.existsSync(path.resolve(`${name}.mp3`))) fs.unlinkSync(path.resolve(`${name}.mp3`)) })
 })
 
 const Server = App.listen(3001, () => {
